4d0a3c12069cc0c57e3e5cf4ea53fd06138ca96e
jhonmaroCIpia <jhonmaro@unicauca.edu.co>
2025-04-09 22:19:34 -0500

    Funcionalidad completa de registro y listado de entidades probado en postman, se modifico el archivo de propiedades para evitar problemas de duplicado de columnas entre otros

--- src/main/java/com/unicauca/pensionados/back_pensionados/capaAccesoADatos/modelos/Entidad.java
+++ src/main/java/com/unicauca/pensionados/back_pensionados/capaAccesoADatos/modelos/Entidad.java
@@ -1,5 +1,43 @@
 package com.unicauca.pensionados.back_pensionados.capaAccesoADatos.modelos;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table (name = "ENTIDAD")
+@Getter @Setter
 public class Entidad {
-    
+    @Id //llave primaria
+    @Column (name = "nitEntidad" , nullable = false, unique = true)
+    private Long nitEntidad;
+
+    @Column (name = "nombreEntidad", nullable = false, length = 100)
+    private String nombreEntidad;
+
+    @Column (name ="direccionEntidad", nullable = false, length =100)
+    private String direccionEntidad;
+
+    @Column (name = "emailEntidad", nullable =  false, length = 100)
+    private String emailEntidad;
+
+    @Column (name = "telefonoEntidad", nullable = false)
+    private Long telefonoEntidad;    
+
+    @Column (name = "estadoEntidad", nullable = false, length = 50)
+    private String estadoEntidad;
+
+    //relacion 1 a muchos Pensonados
+    @OneToMany(mappedBy = "entidadJubilacion")
+    private List<Pensionado> pensionados;
+
+    //relacion 1 a muchos pensionados que trabajaron en la entidad
+    @OneToMany(mappedBy = "entidad")
+    private List<Trabajo> trabajos;
 }
--- src/main/java/com/unicauca/pensionados/back_pensionados/capaPresentacion/controladores/ControladorEntidad.java
+++ src/main/java/com/unicauca/pensionados/back_pensionados/capaPresentacion/controladores/ControladorEntidad.java
@@ -1,5 +1,39 @@
 package com.unicauca.pensionados.back_pensionados.capaPresentacion.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.unicauca.pensionados.back_pensionados.capaAccesoADatos.modelos.Entidad;
+import com.unicauca.pensionados.back_pensionados.CapaServicio.servicios.ServicioEntidad;
+import java.util.List;
+
+@RestController
+@RequestMapping("/entidad")
 public class ControladorEntidad {
-    
+    @Autowired
+    private ServicioEntidad entidadService;
+
+    @PostMapping("/registrar")
+    public Entidad registrarEntidad(@RequestBody Entidad entidad) {
+        return entidadService.registrarEntidad(entidad);
+    }
+
+    @GetMapping("/buscar")
+    public List<Entidad> buscarPorCriterio(@RequestParam(required = false) String query) {
+        if (query == null || query.trim().isEmpty()) {
+            return entidadService.listarTodos();
+        }
+        query = query.replace("\"", "").trim(); // elimina comillas
+        return entidadService.buscarEntidadesPorCriterio(query);
+    }
+
+    @GetMapping("/listar")
+    public List<Entidad> listarTodos() {
+        return entidadService.listarTodos();
+    }
+
+    @PutMapping("/actualizar/{nid}")
+    public Entidad actualizarEntidad(@PathVariable Long id, @RequestBody Entidad entidad) {
+        return entidadService.actualizar(id, entidad);
+    }
 }
