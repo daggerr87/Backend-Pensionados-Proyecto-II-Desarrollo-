728ce22cb7f48cd383801d9f62bb9a36db6eebf7
Juan Camilo Rodallega Mera <94797422+CamiloR293@users.noreply.github.com>
2025-09-16 22:11:43 -0500

    Anotaciones @Operation para documentacion swagger

--- src/main/java/com/unicauca/pensionados/back_pensionados/capaAccesoADatos/repositories/PensionadoRepositorio.java
+++ src/main/java/com/unicauca/pensionados/back_pensionados/capaAccesoADatos/repositories/PensionadoRepositorio.java
@@ -2,6 +2,7 @@ package com.unicauca.pensionados.back_pensionados.capaAccesoADatos.repositories;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,10 +11,26 @@ import com.unicauca.pensionados.back_pensionados.capaAccesoADatos.modelos.Pensio
 import com.unicauca.pensionados.back_pensionados.capaPresentacion.dto.respuesta.EntidadCuotaParteRespuesta;
 
 public interface PensionadoRepositorio extends JpaRepository<Pensionado, Long>{
+    @Operation(
+        summary = "Buscar pensionados por nombre",
+        description = "Este endpoint permite buscar pensionados cuyo nombre contenga una cadena de texto específica, sin distinguir entre mayúsculas y minúsculas."
+    )
     List<Pensionado> findByNombrePersonaContainingIgnoreCase(String query);
+    @Operation(
+        summary = "Buscar pensionados por apellidos",
+        description = "Este endpoint permite buscar pensionados cuyos apellidos contengan una cadena de texto específica, sin distinguir entre mayúsculas y minúsculas."
+    )
     List<Pensionado> findByApellidosPersonaContainingIgnoreCase(String query);
+    @Operation(
+        summary = "Buscar pensionados por nombre o apellidos",
+        description = "Este endpoint permite buscar pensionados cuyo nombre o apellidos contengan una cadena de texto específica, sin distinguir entre mayúsculas y minúsculas."
+    )
     List<Pensionado> findByNombrePersonaContainingIgnoreCaseOrApellidosPersonaContainingIgnoreCase(String nombre, String apellido);
 
+    @Operation(
+            summary = "Obtener entidades y cuota parte total por pensionado",
+            description = "Este endpoint devuelve una lista de entidades junto con la suma total de la cuota parte asociada a un pensionado específico, excluyendo la entidad con NIT 8911500319L."
+    )
     @Query("SELECT new com.unicauca.pensionados.back_pensionados.capaPresentacion.dto.respuesta.EntidadCuotaParteRespuesta(" +
     "t.entidad.nitEntidad, t.entidad.nombreEntidad, SUM(p.cuotaParteTotalAnio)) " +
     "FROM Trabajo t " +
