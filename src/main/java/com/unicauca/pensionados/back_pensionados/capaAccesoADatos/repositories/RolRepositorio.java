4c0ae0eae4758bc44221f736d6bc13832ec805d7
daggerr87 <92834611+daggerr87@users.noreply.github.com>
2025-10-12 15:52:20 -0500

    Update RolRepositorio.java

--- src/main/java/com/unicauca/pensionados/back_pensionados/capaAccesoADatos/repositories/RolRepositorio.java
+++ src/main/java/com/unicauca/pensionados/back_pensionados/capaAccesoADatos/repositories/RolRepositorio.java
@@ -1,5 +1,7 @@
 package com.unicauca.pensionados.back_pensionados.capaAccesoADatos.repositories;
 
+import com.unicauca.pensionados.back_pensionados.capaAccesoADatos.modelos.Rol;
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
